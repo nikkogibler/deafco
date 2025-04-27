3eca5d6411ace50e89266e8048ee054e9382199d
nikkogibler <nikkogibler@gmail.com>
2025-04-27 12:40:08 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,16 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+export default function Home() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push('/login')
+  }, [router])
+
+  return null
+}
+
 export default function Home() {
   return <h1>Deaf Co. SonicSuite</h1>
 }
