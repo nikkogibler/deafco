1e549141d99901939c97028813bdbfbb4173af02
nikkogibler <nikkogibler@gmail.com>
2025-04-27 18:55:51 -0600

    Update dashboard.tsx

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import supabase from '@/lib/supabaseClient'
+import supabase from '@/utils/supabaseClient'
 import { useRouter } from 'next/router'
 
 export default function Dashboard() {
