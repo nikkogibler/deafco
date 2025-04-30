716ce150a193b8194ea350feae1371da04db0e99
nikkogibler <nikkogibler@gmail.com>
2025-04-29 18:16:40 -0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,35 @@
-// pages/_app.tsx
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { AppProps } from 'next/app'
 import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import '../styles/globals.css'
+import { SessionContextProvider, useSessionContext } from '@supabase/auth-helpers-react'
+import { useRouter } from 'next/router'
+import '@/styles/globals.css'
 
-export default function MyApp({ Component, pageProps }) {
-const [supabase] = useState(() => createPagesBrowserClient())
+function AuthRedirectHandler() {
+  const { isLoading, session } = useSessionContext()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!isLoading && session) {
+      const next = new URLSearchParams(window.location.search).get('next')
+      if (next) {
+        router.replace(next)
+      }
+    }
+  }, [isLoading, session, router])
+
+  return null
+}
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [supabaseClient] = useState(() => createPagesBrowserClient())
 
   return (
-    <SessionContextProvider
-      supabaseClient={supabase}
-      initialSession={pageProps.initialSession}
-    >
-      <div className="bg-gray-900 text-white min-h-screen">
-        <Component {...pageProps} />
-      </div>
+    <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
+      <AuthRedirectHandler />
+      <Component {...pageProps} />
     </SessionContextProvider>
   )
 }
+
+export default MyApp
