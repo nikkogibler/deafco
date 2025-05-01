dc8e69131283112267c8871370f1577fff754067
nikkogibler <nikkogibler@gmail.com>
2025-05-01 14:06:22 -0600

    Update login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -43,7 +43,7 @@ export default function Login() {
             'streaming',
             'user-library-read'
           ].join(' '),
-          redirectTo: window.location.origin + '/callback',
+          redirectTo: window.location.origin + '/dashboard',
           queryParams: {
             // Explicitly request offline access to get refresh token
             prompt: 'consent'
@@ -85,3 +85,4 @@ export default function Login() {
     </div>
   )
 }
+
